9b943f3a70b6db7dd7c262d992e6b778e2a66678
Joakim Plate <elupus@ecce.se>
2012-10-21 17:33:45 +0200

    paplayer: bring OnPlaybackStarted/Stopped in line with dvdplayer

--- xbmc/cores/paplayer/PAPlayer.cpp
+++ xbmc/cores/paplayer/PAPlayer.cpp
@@ -70,12 +70,7 @@ PAPlayer::PAPlayer(IPlayerCallback& callback) :
 
 PAPlayer::~PAPlayer()
 {
-  if (!m_isPaused)
-    SoftStop(true, true);
-  CloseAllStreams(false);
-
-  /* wait for the thread to terminate */
-  StopThread(true);//true - wait for end of thread
+  CloseFile();
   delete m_FileItem;
 }
 
@@ -244,6 +239,7 @@ bool PAPlayer::OpenFile(const CFileItem& file, const CPlayerOptions &options)
   if (m_streams.size() > 1 || !m_defaultCrossfadeMS || m_isPaused)
   {
     CloseAllStreams(!m_isPaused);
+    StopThread();
     m_isPaused = false; // Make sure to reset the pause state
   }
 
@@ -472,7 +468,12 @@ inline bool PAPlayer::PrepareStream(StreamInfo *si)
 
 bool PAPlayer::CloseFile()
 {
-  m_callback.OnPlayBackStopped();
+  if (!m_isPaused)
+    SoftStop(true, true);
+  CloseAllStreams(false);
+
+  /* wait for the thread to terminate */
+  StopThread(true);//true - wait for end of thread
   return true;
 }
 
@@ -515,6 +516,11 @@ void PAPlayer::Process()
 
     GetTimeInternal(); //update for GUI
   }
+
+  if(m_isFinished && !m_bStop)
+    m_callback.OnPlayBackEnded();
+  else
+    m_callback.OnPlayBackStopped();
 }
 
 inline void PAPlayer::ProcessStreams(double &delay, double &buffer)
@@ -524,7 +530,6 @@ inline void PAPlayer::ProcessStreams(double &delay, double &buffer)
   {
     m_isPlaying = false;
     delay       = 0;
-    m_callback.OnPlayBackEnded();
     return;
   }
 
