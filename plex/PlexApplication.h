cf3b79910f931d424dad3f7a1345bde5883c06ec
Lionel CHAZALLON <LongChair@hotmail.com>
2014-06-16 21:15:53 +0200

    Fix Post Merge build issue

--- plex/PlexApplication.h
+++ plex/PlexApplication.h
@@ -123,7 +123,6 @@ public:
   CPlexPlayQueueManagerPtr playQueueManager;
   CPlexDirectoryCachePtr directoryCache;
   CPlexBusyIndicator busy;
-  CPlexDirectoryCachePtr directoryCache;
 
   void setNetworkLogging(bool);
   void OnTimeout();
