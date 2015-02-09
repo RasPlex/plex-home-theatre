5d323a8606330d80750824503d14d78990b37673
Dale Hamel <dale.hamel@srvthe.net>
2015-02-09 12:17:03 +0000

    Disable AVIO by default until we can fix t/c bug

--- plex/Owned/GUISettings.cpp
+++ plex/Owned/GUISettings.cpp
@@ -903,7 +903,7 @@ void CGUISettings::Initialize()
 #endif
 
 #ifdef TARGET_RASPBERRY_PI
-  AddBool(adv, "videoplayer.useffmpegavio", 44406, true);
+  AddBool(adv, "videoplayer.useffmpegavio", 44406, false);
 #else
   AddBool(adv, "videoplayer.useffmpegavio", 44406, false);
 #endif
