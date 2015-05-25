xbmc/xbmc

dvdplayer: adapt matroshka ssa subs to ffmpeg 2.4 api change

xbmc/cores/dvdplayer/DVDCodecs/Overlay/DVDOverlayCodecSSA.cpp
@@ -45,7 +45,8 @@ CDVDOverlayCodecSSA::~CDVDOverlayCodecSSA()
 
 bool CDVDOverlayCodecSSA::Open(CDVDStreamInfo &hints, CDVDCodecOptions &options)
 {
-  if(hints.codec != AV_CODEC_ID_SSA)
+  if(hints.codec != AV_CODEC_ID_SSA &&
+     hints.codec != AV_CODEC_ID_ASS)
     return false;
 
   Dispose();

RasPlex/plex-home-theatre

xbmc/cores/dvdplayer/DVDCodecs/Overlay/DVDOverlayCodecSSA.cpp
@@ -44,7 +44,8 @@ CDVDOverlayCodecSSA::~CDVDOverlayCodecSSA()
 
 bool CDVDOverlayCodecSSA::Open(CDVDStreamInfo &hints, CDVDCodecOptions &options)
 {
-  if(hints.codec != CODEC_ID_SSA)
+  if(hints.codec != CODEC_ID_SSA &&
+     hints.codec != AV_CODEC_ID_ASS)
     return false;
 
   Dispose();