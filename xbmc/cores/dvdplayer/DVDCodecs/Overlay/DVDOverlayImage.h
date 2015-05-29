xbmc/xbmc

[bluray] Cosmetics: emphasize where the used values come from.

xbmc/cores/dvdplayer/DVDCodecs/Overlay/DVDOverlayImage.h
@@ -92,14 +92,14 @@ public:
     source_width   = src.source_width;
     source_height  = src.source_height;
 
-    data = (uint8_t*)malloc(height*linesize);
+    data = (uint8_t*)malloc(sub_h * linesize);
 
     uint8_t* s = src.data_at(sub_x, sub_y);
     uint8_t* t = data;
 
     for(int row = 0;row < sub_h; ++row)
     {
-      memcpy(t, s, width*bpp);
+      memcpy(t, s, linesize);
       s += src.linesize;
       t += linesize;
     }

RasPlex/plex-home-theatre

xbmc/cores/dvdplayer/DVDCodecs/Overlay/DVDOverlayImage.h
@@ -92,14 +92,14 @@ public:
     source_width   = src.source_width;
     source_height  = src.source_height;
 
-    data = (BYTE*)malloc(height*linesize);
+    data = (BYTE*)malloc(sub_h * linesize);
 
     BYTE* s = src.data_at(sub_x, sub_y);
     BYTE* t = data;
 
     for(int row = 0;row < sub_h; ++row)
     {
-      memcpy(t, s, width*bpp);
+      memcpy(t, s, linesize);
       s += src.linesize;
       t += linesize;
     }