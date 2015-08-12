ulion/xbmc

Add method to remove specified thread message in the message queue.

xbmc/guilib/GUIWindowManager.cpp
@@ -825,6 +825,32 @@ void CGUIWindowManager::DispatchThreadMessages()
   }
 }
 
+int CGUIWindowManager::RemoveThreadMessageByMessageIds(int *pMessageIDList)
+{
+  CSingleLock lock(m_critSection);
+  int removedMsgCount = 0;
+  for (std::list <struct GUIThreadMessage_t>::iterator it = m_vecThreadMessages.begin();
+       it != m_vecThreadMessages.end();)
+  {
+    CGUIMessage *pMsg = it->pMsg;
+    int *pMsgID;
+    for(pMsgID = pMessageIDList; *pMsgID != 0; ++pMsgID)
+      if (pMsg->GetMessage() == *pMsgID)
+        break;
+    if (*pMsgID)
+    {
+      it = m_vecThreadMessages.erase(it);
+      delete pMsg;
+      ++removedMsgCount;
+    }
+    else
+    {
+      ++it;
+    }
+  }
+  return removedMsgCount;
+}
+
 void CGUIWindowManager::AddMsgTarget( IMsgTargetCallback* pMsgTarget )
 {
   m_vecMsgTargets.push_back( pMsgTarget );

RasPlex/plex-home-theatre

xbmc/guilib/GUIWindowManager.cpp
@@ -815,33 +815,6 @@ int CGUIWindowManager::GetTopMostModalDialogID(bool ignoreClosing /*= false*/) c
   return WINDOW_INVALID;
 }
 
-/* PLEX */
-int CGUIWindowManager::RemoveThreadMessageByMessageIds(int *pMessageIDList)
-{
-  CSingleLock lock(m_critSection);
-  int removedMsgCount = 0;
-  for (std::vector < std::pair<CGUIMessage*,int> >::iterator it = m_vecThreadMessages.begin(); it != m_vecThreadMessages.end();)
-  {
-    CGUIMessage *pMsg = it->first;
-    int *pMsgID;
-    for(pMsgID = pMessageIDList; *pMsgID != 0; ++pMsgID)
-      if (pMsg->GetMessage() == *pMsgID)
-        break;
-    if (*pMsgID)
-    {
-      it = m_vecThreadMessages.erase(it);
-      delete pMsg;
-      ++removedMsgCount;
-    }
-    else
-    {
-      ++it;
-    }
-  }
-  return removedMsgCount;
-}
-/* END PLEX */
-
 void CGUIWindowManager::SendThreadMessage(CGUIMessage& message, int window /*= 0*/)
 {
   CSingleLock lock(m_critSection);
@@ -892,6 +865,32 @@ void CGUIWindowManager::DispatchThreadMessages()
 
     lock.Enter();
   }
+}
+
+int CGUIWindowManager::RemoveThreadMessageByMessageIds(int *pMessageIDList)
+{
+  CSingleLock lock(m_critSection);
+  int removedMsgCount = 0;
+  for (std::list < std::pair<CGUIMessage*,int> >::iterator it = m_vecThreadMessages.begin();
+       it != m_vecThreadMessages.end();)
+  {
+    CGUIMessage *pMsg = it->first;
+    int *pMsgID;
+    for(pMsgID = pMessageIDList; *pMsgID != 0; ++pMsgID)
+      if (pMsg->GetMessage() == *pMsgID)
+        break;
+    if (*pMsgID)
+    {
+      it = m_vecThreadMessages.erase(it);
+      delete pMsg;
+      ++removedMsgCount;
+    }
+    else
+    {
+      ++it;
+    }
+  }
+  return removedMsgCount;
 }
 
 void CGUIWindowManager::AddMsgTarget( IMsgTargetCallback* pMsgTarget )